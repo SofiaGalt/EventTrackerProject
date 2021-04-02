520fe411ed26891451b4268e6079d32bd39178de
Sofia <sgkartist@gmail.com>
2021-04-02 15:06:11 -0600

    Spring Starter Project initialized.

---
+++ ZetaCircuit/src/main/java/com/skilldistillery/runner/ZetaCircuitApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZetaCircuitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZetaCircuitApplication.class, args);
+	}
+
+}
---
+++ ZetaCircuit/src/test/java/com/skilldistillery/runner/ZetaCircuitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZetaCircuitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
