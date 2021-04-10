868088ffcd06a48040c11537124e2a22fcf31838
Sofia <sgkartist@gmail.com>
2021-04-10 12:41:24 -0600

    Login logout.

--- ZetaCircuit/src/main/java/com/skilldistillery/runner/controllers/UserController.java
+++ ZetaCircuit/src/main/java/com/skilldistillery/runner/controllers/UserController.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.runner.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.runner.entities.User;
 import com.skilldistillery.runner.services.UserService;
 
 @RequestMapping("api")
@@ -12,4 +18,14 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@GetMapping("users/{id}")
+	public User retrieve(@PathVariable Integer id, HttpServletResponse resp, HttpServletRequest req) {
+		
+		User u = userService.retrieve(id);
+		if(u == null) {
+			resp.setStatus(404);
+		}
+		return u;
+	}
 }
--- ZetaCircuit/src/main/resources/static/js/script.js
+++ ZetaCircuit/src/main/resources/static/js/script.js
@@ -1,3 +1,5 @@
+let user = {};
+
 window.addEventListener('load', function (evt) {
     console.log("script.js loaded");
     init();
@@ -5,6 +7,80 @@ window.addEventListener('load', function (evt) {
 
 function init(){
     loadZetaCircuit();
+    userPrompt();
+}
+
+function userPrompt(){
+    let div = document.querySelector('#currentUser');
+    div.innerHTML = '';
+    var submitButton = document.createElement('input');
+    submitButton.type = "submit";
+    submitButton.value= "Login";
+    submitButton.addEventListener('click', login);
+    div.appendChild(submitButton);
+}
+
+function login(){
+    
+    let div = document.querySelector('#currentUser');
+    div.innerHTML = "";
+
+    let getUserId = document.createElement('form');
+    getUserId.name = 'getUserId';
+    div.appendChild(getUserId);
+    let label = document.createElement('label');
+    getUserId.appendChild(label);
+    let input = document.createElement('input');
+    input.name = 'id';
+    getUserId.appendChild(input);
+    
+    var submitButton = document.createElement('input');
+    submitButton.type = "submit";
+    submitButton.value= "Submit";
+    submitButton.addEventListener('click', setUser);
+    getUserId.appendChild(submitButton);
+}
+
+function setUser(e){
+
+    e.preventDefault();
+    console.log('inside setUser');
+    let form = document.getUserId;
+    console.log(form.id.value);
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", `api/users/${form.id.value}`);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                
+                user = JSON.parse(xhr.responseText);
+                console.log(user);
+                displayUser();
+            } else {
+                let div = document.querySelector('#currentUser');
+                let append = document.createElement('p');
+                append.innerHTML = `We couldn't retrieve a user using that id.`;
+                div.appendChild(append);
+            }
+        }
+    }
+    xhr.send();
+}
+
+function displayUser(){
+    let div = document.querySelector('#currentUser');
+    div.innerHTML = "";
+
+    console.log("inside displayUser()");
+
+    div.innerHTML = 'Logged in as ' + user.username + '<br>';
+
+    var logout = document.createElement('input');
+    logout.type = "submit";
+    logout.value= "logout";
+    logout.addEventListener('click', userPrompt);
+    div.appendChild(logout);
 }
 
 function loadZetaCircuit(){
@@ -20,7 +96,7 @@ function loadZetaCircuit(){
                 console.log(runs);
                 displayRuns(runs);
             } else {
-                displayError('Error retrieving runs: ' + xhr.status)
+                displayErrors('Error retrieving runs: ' + xhr.status);
             }
         }
     }
@@ -30,7 +106,7 @@ function loadZetaCircuit(){
 function displayErrors(msg){
     let div = document.querySelector('#errors');
     let h1 = document.createElement('h3');
-    h1.textContent(msg);
+    h1.textContent = msg;
     div.appendChild(h1);
 }
 
