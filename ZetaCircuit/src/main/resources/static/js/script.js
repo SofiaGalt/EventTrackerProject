1863988d4356ac92be28c13669d4e4e4610adc3f
Sofia <sgkartist@gmail.com>
2021-04-10 09:01:52 -0600

    displayRun() contains all fields.

--- ZetaCircuit/src/main/resources/static/js/script.js
+++ ZetaCircuit/src/main/resources/static/js/script.js
@@ -17,6 +17,7 @@ function loadZetaCircuit(){
             if(xhr.status === 200){
                 console.log(' successful get request! ***');
                 let runs = JSON.parse(xhr.responseText);
+                console.log(runs);
                 displayRuns(runs);
             } else {
                 displayError('Error retrieving runs: ' + xhr.status)
@@ -36,10 +37,44 @@ function displayErrors(msg){
 function displayRuns(runs){
 
     console.log('display runs');
- let div = document.querySelector('#runEntriesTable');
- for (const run of runs) {
-     let li = document.createElement('li');
-     li.textContent = run.distance;
-     div.appendChild(li);
- }
+
+    for (const run of runs) {
+        displayRun(run);
+    }
+}
+
+function displayRun(run){
+    
+    let div = document.querySelector('#runEntriesTable');
+    let li = document.createElement('li');
+    let divListItem = document.createElement('div');
+    if(run.raceTitle) li.innerHTML = li.innerHTML + `race: ${run.raceTitle}  `;
+    if(run.location) li.innerHTML = li.innerHTML + `location: ${run.location}   `
+    if(run.distance) {
+        li.innerHTML = li.innerHTML + `distance: ${run.distance}`;
+        if(run.distanceUnit) {
+            li.innerHTML = li.innerHTML + `${run.distanceUnit}   `;
+        }
+    }
+    if(run.hours || run.minutes || run.seconds) li.innerHTML = li.innerHTML + `time: `
+    if(run.hours){ 
+        li.innerHTML = li.innerHTML + `${run.hours}:`;
+    } else {
+        li.innerHTML = li.innerHTML + `00:`;
+    }
+    if(run.minutes) {
+        li.innerHTML = li.innerHTML + `${run.minutes}:`;
+    } else {
+        li.innerHTML = li.innerHTML + `00:`;
+    }
+    if(run.seconds) {
+        li.innerHTML = li.innerHTML + `${run.seconds}  `;
+    } else {
+        li.innerHTML = li.innerHTML + `00  `;
+    }
+    if(run.notes){
+        li.innerHTML = li.innerHTML + `<br>${run.notes}`
+    }
+    li.innerHTML = li.innerHTML + `<br>Posted By ${run.user.username}`;
+    div.appendChild(li);
 }
